00217aba24cdf46ac589263626cb36b5a3b00978
Niranjan TK <92922347+Niranjantk@users.noreply.github.com>
2025-07-01 14:30:59 +0530

    new try

--- lib/widgets/cardview.dart
+++ lib/widgets/cardview.dart
@@ -1,8 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Cardview extends StatelessWidget {
+import 'package:flutter/material.dart';
+//import http
+import 'package:http/http.dart' as http;
+
+class Cardview extends StatefulWidget {
   const Cardview({super.key});
 
+  @override
+  State<Cardview> createState() => _CardviewState();
+}
+
+
+
+class _CardviewState extends State<Cardview> {
+  // Future <Map<String,dynamic>> getData() async{
+  //   final response = await http.get(Uri.parse('http://10.0.2.2:3000/balance'));
+  //   return jsonDecode(response.body)['balance'];
+  // }
+   Future<Map<String, dynamic>> getData() async {
+  try {
+    final response = await http.get(Uri.parse('http://10.0.2.2:3000/balance'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['balance'];
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (e) {
+    print("Error fetching data: $e");
+    return {};
+  }
+}
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,14 +55,23 @@ class Cardview extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 0, 40, 10),
-                child: const Text(
-                  "\$3,200.00",
-
-                  style: TextStyle(
-                    //fontFamily: "SpaceGrotesk",
-                    fontSize: 48,
-                    fontWeight: FontWeight.w900,
-                  ),
+                child: FutureBuilder(
+                  future: getData(),
+                  builder: (context, snapshot) {
+                    if(snapshot.hasData){
+                      return  Text((snapshot.data as Map)['amount'].toString(),
+                      style: TextStyle(
+                        //fontFamily: "SpaceGrotesk",
+                      fontSize: 48,
+                      fontWeight: FontWeight.w900,
+                      ),
+                    );
+                    }
+                    else{
+                      return CircularProgressIndicator();
+                    }
+                    
+                  }
                 ),
               ),
               Padding(
